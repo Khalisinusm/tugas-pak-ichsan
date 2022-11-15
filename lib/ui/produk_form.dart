3c165aff41b4a4a6b3f39301d316953537c53df6
rahmat <RAHMATKHALISIN108@gmail.com>
2022-11-15 13:09:50 +0700

    menambahkan fitur save

--- lib/ui/produk_form.dart
+++ lib/ui/produk_form.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/ui/produk_detail.dart';
 
 class ProdukForm extends StatefulWidget {
-  const ProdukForm({super.key});
+  const ProdukForm({Key? key}) : super(key: key);
 
   @override
-  State<ProdukForm> createState() => _ProdukFormState();
+  _ProdukFormState createState() => _ProdukFormState();
 }
 
 class _ProdukFormState extends State<ProdukForm> {
+  final _kodeProdukTextboxController = TextEditingController();
+  final _namaProdukTextboxController = TextEditingController();
+  final _hargaProdukTextboxController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,17 +20,52 @@ class _ProdukFormState extends State<ProdukForm> {
         title: const Text('From Produk'),
       ),
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          TextField(
-              decoration: const InputDecoration(labelText: "kode produk")),
-          TextField(
-              decoration: const InputDecoration(labelText: "nama produk")),
-          TextField(
-              decoration: const InputDecoration(labelText: "harga produk")),
-          ElevatedButton(onPressed: () {}, child: const Text('simpan'))
-        ],
-      )),
+        child: Column(
+          children: [
+            _textboxkodeproduk(),
+            _textboxnamaproduk(),
+            _textboxhargaproduk(),
+            _tombolsimpan()
+          ],
+        ),
+      ),
     );
+  }
+
+  _textboxkodeproduk() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "kode produk"),
+      controller: _kodeProdukTextboxController,
+    );
+  }
+
+  _textboxnamaproduk() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "nama produk"),
+      controller: _namaProdukTextboxController,
+    );
+  }
+
+  _textboxhargaproduk() {
+    return TextField(
+      decoration: const InputDecoration(labelText: "harga produk"),
+      controller: _hargaProdukTextboxController,
+    );
+  }
+
+  _tombolsimpan() {
+    return ElevatedButton(
+        onPressed: () {
+          String kodeProduk = _kodeProdukTextboxController.text;
+          String namaProduk = _namaProdukTextboxController.text;
+          int harga = int.parse(_hargaProdukTextboxController.text);
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => ProdukDetail(
+                    kodeProduk: kodeProduk,
+                    namaProduk: namaProduk,
+                    harga: harga,
+                  )));
+        },
+        child: const Text('simpan'));
   }
 }
